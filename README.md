31f55264a518310cff7842dd66c433a6346a495b
zetherz <dev@zetherz.com>
2018-07-22 20:42:04 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# openzeppelin-solidity-starter
+Start a blank, fully updated, openzeppelin-solidity-based project instantly.
---
+++ _INSTALL-EXAMPLE.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+./_INSTALL.sh -u "MY-GITHUB-USERNAME" -v "MY-GITHUB-EMAIL" -n "MY-PROJ-NAME" -d "MY PROJ DESC" -k "'MY','PROJ','KEYWORDS'" -h "https://github.com/MY-GITHUB-USERNAME/MY-PROJ-NAME" -r "https://github.com/MY-GITHUB-USERNAME/MY-PROJ-NAME.git" -b "https://github.com/MY-GITHUB-USERNAME/MY-PROJ-NAME/issues"
